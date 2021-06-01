d9a2fb0d2c1c53bbc8042c81b08c49084168e9cd
cass1618 <cass16180@gmail.com>
2021-06-01 14:21:19 -0700

    :wrench: Add program, startup, and csproj file and migrations

--- RecipeBox/Models/CategoryRecipe.cs
+++ RecipeBox/Models/CategoryRecipe.cs
@@ -2,7 +2,7 @@ namespace RecipeBox.Models
 {
   public class CategoryRecipe
   {
-    public int CategoryRecipe { get; set; }
+    public int CategoryRecipeId { get; set; }
     public int RecipeId { get; set; }
     public int CategoryId { get; set; }
     public virtual Recipe Recipe { get; set; }
